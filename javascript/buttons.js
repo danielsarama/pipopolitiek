884e191c1a05a828cc3beeffccecc3e8e12bc170
danielsarama <danielsarama432@gmail.com>
2026-03-18 11:20:51 +0100

    test

---
+++ javascript/buttons.js
@@ -0,0 +1,30 @@
+const _FIRST_SCREEN = document.querySelector("first_screen");
+const _SECOND_SCREEN = document.querySelector(".second_screen");
+const _START_BUTTON = document.querySelector(".start_button")
+
+let pageNumbers = 0
+
+_START_BUTTON.addEventListener("click", startScreen)
+
+
+
+function startScreen(){
+
+    switch(pageNumbers){
+
+        case "0":
+        if (_FIRST_SCREEN){
+            _FIRST_SCREEN.style.display = "none"
+            _SECOND_SCREEN.style.display = "flex"
+            pageNumbers++
+    }
+        break;
+
+        case "1":
+            
+
+
+    }
+
+
+}
